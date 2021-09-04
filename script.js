97beb09b1338d25e5f089b6b03f358e59c464d24
Alex J. Lee <alexjlee.dev@gmail.com>
2021-09-04 15:52:22 +0900

    Feature display number when a number btn is clicked

---
+++ script.js
@@ -0,0 +1,19 @@
+const calculator = document.querySelector(".calculator");
+const buttons = calculator.querySelector(".calculator__buttons");
+const displayResult = calculator.querySelector(".calculator__display__result");
+
+let current = "0";
+
+buttons.addEventListener("click", (e) => {
+  const buttonType = e.target.classList[1];
+  const buttonContet = e.target.textContent;
+
+  // When a number button is clicked
+  if (buttonType === "number") {
+    if (current === "0") {
+      current = "";
+    }
+    current += buttonContet;
+    displayResult.textContent = current;
+  }
+});
